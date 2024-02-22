849285b4fefad390f4530ef4412a05088db22177
DK <dongwook5478@gmail.com>
2024-02-22 17:54:12 +0900

    update java note

--- java_note/src/Main.java
+++ java_note/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> three_dice= new ArrayList<Integer>(3);
+        for(int i=0;i<3;i++){
+            three_dice.add((int)(Math.random()*6)+1);
+            System.out.println(three_dice.get(i));
+        }
+        if((three_dice.get(0)==three_dice.get(1)) && (three_dice.get(0)==three_dice.get(2))){
+            System.out.println(three_dice.get(0)+(10000*1000));
+        }
+        if(three_dice.get(0)==three_dice.get(1));
+
+
+
+
+
     }
 }
