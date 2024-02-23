71e4e4452c77d8ba5c3fc670b1b077b3049347ac
DK <dongwook5478@gmail.com>
2024-02-23 09:39:48 +0900

    seperate problem from Main fa sd

--- java_note/src/Main.java
+++ java_note/src/Main.java
@@ -2,9 +2,13 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
 
+        System.out.println(getScore());
+
+    }
+    public static int getScore(){
         int intMax=0;
         int[] num=new int[3];
 
@@ -12,32 +16,6 @@ public class Main {
         String input=sc.nextLine();
         String[] sNum=input.split(" ");
 
-//        change String to int
-        for(int i=0;i<num.length;i++){
-            num[i]=Integer.parseInt(sNum[i]);
-            if(i==0){
-                intMax=num[i];
-            }
-            else if(intMax<num[i]){
-                intMax=num[i];
-            }
-        }
-
-        if((num[0]==num[1]) && (num[0])==num[2]){
-            System.out.println((num[0]*1000)+10000);
-        }
-        else if(num[0]==num[1] || num[0]==num[2] || num[2]==num[1]){
-            if(num[0]==num[1]){
-                System.out.println(num[0]*100+1000);
-            } else if (num[0]==num[2]) {
-                System.out.println(num[0]*100+1000);
-            }
-            else{
-                System.out.println(num[1]*100+1000);
-            }
-        }
-        else{
-            System.out.println(intMax*100);
-        }
+        return ThreeDice.calcScore(sNum,intMax);
     }
 }
--- java_note/src/ThreeDice.java
+++ java_note/src/ThreeDice.java
@@ -1,2 +1,36 @@
 public class ThreeDice {
+    public static int calcScore(String[] sNum, int maxNum){
+        int score=0;
+        int[] num=new int[3];
+
+        //        change String to int
+        for(int i=0;i<num.length;i++){
+            num[i]=Integer.parseInt(sNum[i]);
+            if(i==0){
+                maxNum=num[i];
+            }
+            else if(maxNum<num[i]){
+                maxNum=num[i];
+            }
+        }
+
+        if((num[0]==num[1]) && (num[0])==num[2]){
+            score=((num[0]*1000)+10000);
+        }
+        else if(num[0]==num[1] || num[0]==num[2] || num[2]==num[1]){
+            if(num[0]==num[1]){
+                score=(num[0]*100+1000);
+            } else if (num[0]==num[2]) {
+                score=(num[0]*100+1000);
+            }
+            else{
+                score=(num[1]*100+1000);
+            }
+        }
+        else{
+            score=(maxNum*100);
+        }
+
+        return score;
+    }
 }
