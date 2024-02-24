07d44b8737b8a9219eb1e1f4de2b8efcf7e3602a
DK <dongwook5478@gmail.com>
2024-02-24 13:40:32 +0900

    add class: PalindromeNumber

--- java_note/src/Main.java
+++ java_note/src/Main.java
@@ -4,6 +4,35 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
+        int x=900;
+        String xStr=String.valueOf(x);
+        int len=xStr.length();
+        String[] arrStr=xStr.split("");
+
+        boolean tf;
+        for (int i=0;i<len;i++){
+            System.out.println(arrStr[i]);
+        }
+
+        for (int i=0;i<len/2;i++){
+            if(arrStr[i].equals(arrStr[len-i-1])){
+
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+
+
+
+//        for (int i=0;i<len/2;i++){
+//            if()
+//        }
+
+
+
+
 
 //        ThreeDice
 //        System.out.println(getScore());
